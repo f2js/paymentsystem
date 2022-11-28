0d9886e167098f752fd59bbf8b5492d4ac557ab7
Frederik Dahl <cph-fd76@cphbusiness.dk>
2022-11-28 10:28:41 +0100

    initial gRPC setup

---
+++ src/test/java/exam/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
