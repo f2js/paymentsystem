4a9f6b507d615b2928c04bbe407e6288c56c9fa8
Frederik Dahl <cph-fd76@cphbusiness.dk>
2022-11-29 10:58:44 +0100

    Made payment fail occasionally
    25% of the time the payment will fail

--- src/main/java/paymentsystem/PaymentService.java
+++ src/main/java/paymentsystem/PaymentService.java
@@ -5,6 +5,8 @@ import grpc.paymentGrpc.paymentImplBase;
 import io.grpc.stub.StreamObserver;
 import grpc.PaymentSystem.APIResponse;
 
+import java.util.Random;
+
 public class PaymentService extends paymentImplBase {
     @Override
     public void payment(PaymentSystem.PaymentRequest request, StreamObserver<PaymentSystem.APIResponse> responseObserver) {
@@ -13,9 +15,18 @@ public class PaymentService extends paymentImplBase {
 
         String stringResponse ="OrderId: " + orderId + ", orderPrice: " + orderPrice + " kr";
 
+        //Make interface "fail occasionally"
+        Random rand = new Random(); //instance of random class
+        int upperbound = 4;
+        int int_random = rand.nextInt(upperbound);
+
         APIResponse.Builder response = APIResponse.newBuilder();
         if(orderId != null || orderPrice != null){
-            response.setResponseCode(0).setResponsemessage("|SUCCESS| " + stringResponse );
+            if(int_random == 0){
+                response.setResponseCode(100).setResponsemessage("|PAYMENT FAILURE|");
+            } else {
+                response.setResponseCode(0).setResponsemessage("|PAYMENT SUCCESS| " + stringResponse );
+            }
         } else {
             response.setResponseCode(100).setResponsemessage("|FAILURE| " + stringResponse);
         }
